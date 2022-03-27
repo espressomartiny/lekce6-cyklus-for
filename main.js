bb4df6cf154fd22d4479cb7909adb566adf37a1d
Martina Vokáčová <martinavokacova@gmail.com>
2022-03-27 12:11:27 +0200

    --

--- main.js
+++ main.js
@@ -36,9 +36,9 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-// let c = 10;
-// while (c >= 0) {
-//   console.log(c);
-//   c - 1;
-// }
+let c = 10;
+ while (c >= 0) {
+ console.log(c);
+ c--;
+}
 console.log('-------------------');
