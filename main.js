701573b91fd1c2ab2c3f6cb2fc609e0a9449ee0f
Martina Vokáčová <martinavokacova@gmail.com>
2022-03-27 11:55:00 +0200

    úprava formátování

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
-
 // První příklad - vypiš vzestupně čísla od 0 do 10
-console.log('První příklad - vzestupná řada 0 - 10');
+console.log("První příklad - vzestupná řada 0 - 10");
 
 //TADY NAPIS CYKLUS
 
 for (let i = 0; i <= 10; i = i + 1) {
   console.log(i);
 }
-console.log('-------------------');
-
-
+console.log("-------------------");
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
 // v podmínce uvnitř cyklu
 
-console.log('Druhý příklad - rozdíl mezi <= a <');
+console.log("Druhý příklad - rozdíl mezi <= a <");
 
 //TADY NAPIS CYKLUS
 
 for (let i = 0; i < 10; i = i + 1) {
-    console.log(i);
-  }
-console.log('-------------------');
-
-
+  console.log(i);
+}
+console.log("-------------------");
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-console.log('Třetí příklad - sestupná řada 10 - 0');
+console.log("Třetí příklad - sestupná řada 10 - 0");
 
 //TADY NAPIS CYKLUS
 
 for (let i = 10; i >= 0; i = i - 1) {
-    console.log(i);
-  }
-console.log('-------------------');
+  console.log(i);
+}
+console.log("-------------------");
